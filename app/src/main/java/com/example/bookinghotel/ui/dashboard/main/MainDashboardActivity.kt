d7fa7b7d9c7edf034a5e7954f42f8e525134dc4d
oliwier <oliviersakwa@gmail.com>
2021-11-30 20:25:22 +0100

    lukassakwa Main Dashboard Activity

---
+++ app/src/main/java/com/example/bookinghotel/ui/dashboard/main/MainDashboardActivity.kt
@@ -0,0 +1,16 @@
+package com.example.bookinghotel.ui.dashboard.main
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bookinghotel.R
+
+class MainDashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_dashboard_activity_layout)
+    }
+
+}
