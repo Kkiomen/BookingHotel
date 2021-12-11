46d25d9c5bf58a293265ae85aa9bf1aca1e831a3
oliwier <oliviersakwa@gmail.com>
2021-12-11 12:35:30 +0100

    Register View Model Class

--- app/src/main/java/com/example/bookinghotel/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/bookinghotel/ui/register/RegisterViewModel.kt
@@ -2,10 +2,13 @@ package com.example.bookinghotel.ui.register
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.bookinghotel.ui.login.LoginViewModel
 import com.example.bookinghotel.ui.register.RegisterRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -15,21 +18,30 @@ class RegisterViewModel @Inject constructor(
     private val registerRepository: RegisterRepository
 ) : ViewModel() {
 
+    sealed class RegisterState(){
+        object Empty : RegisterState()
+        object Success : RegisterState()
+        data class Error(val message : String) : RegisterState()
+    }
+
+    //add validation
     var email = ""
     var password = ""
 
+    private val _registerState = MutableStateFlow<RegisterState>(RegisterState.Empty)
+    val registerState : StateFlow<RegisterState> = _registerState
+
     fun registerUser(){
         CoroutineScope(Dispatchers.IO).launch{
+            _registerState.value = RegisterState.Empty
             try {
                 registerRepository.createUser(email, password)
                 withContext(Dispatchers.IO){
-                    //TODO:: send info to user ui "Success to create user account"
-                    Log.i("REGISTER SUCCESS", "user register in")
+                    _registerState.value = RegisterState.Success
                 }
             }catch (e : Exception){
                 withContext(Dispatchers.Main){
-                    //TODO:: send info to user ui "Failed to create user account"
-                    Log.i("REGISTER FAILURE", "user could not register in")
+                    _registerState.value = RegisterState.Error("Could not create user")
                 }
             }
         }
