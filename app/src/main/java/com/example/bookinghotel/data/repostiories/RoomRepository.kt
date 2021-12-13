cfbdd2de1f3cbcb324c68c749c53c2b97f146957
oliwier <oliviersakwa@gmail.com>
2021-12-13 22:15:18 +0100

    Firebase Room Model Repository Class (findAll() etc...)

---
+++ app/src/main/java/com/example/bookinghotel/data/repostiories/RoomRepository.kt
@@ -0,0 +1,25 @@
+package com.example.bookinghotel.data.repostiories
+
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.Source
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
+
+class RoomRepository {
+
+    private val firestore = Firebase.firestore
+    private val source = Source.CACHE
+
+    suspend fun findRoomById(hotelDocumentId : String): DocumentSnapshot? {
+        return firestore.collection("room").document(hotelDocumentId).get(source).await()
+    }
+    suspend fun findAllRooms(){
+        firestore.collection("room").get(source).await()
+    }
+
+    fun addRoom(documentId : Int){
+        firestore.collection("room").document(documentId.toString())
+    }
+
+}
