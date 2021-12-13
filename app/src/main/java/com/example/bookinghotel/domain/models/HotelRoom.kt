62f06de6cf5fec481c418fad4957696d3499f016
oliwier <oliviersakwa@gmail.com>
2021-12-13 22:13:20 +0100

    Domain HotelRoom Model Class (Model which we use in app)

--- app/src/main/java/com/example/bookinghotel/domain/models/HotelRoom.kt
+++ app/src/main/java/com/example/bookinghotel/domain/models/HotelRoom.kt
@@ -3,11 +3,12 @@ package com.example.bookinghotel.domain.models
 import com.example.bookinghotel.data.models.Room
 
 data class HotelRoom(
-    val uid: Int? = null,
     val name: String? = null,
+    val city: String? = null,
     val address1: String? = null,
     val address2: String? = null,
     val address3: String? = null,
-    val city: String? = null,
     val rooms: List<Room>? = null
 )
+
+
