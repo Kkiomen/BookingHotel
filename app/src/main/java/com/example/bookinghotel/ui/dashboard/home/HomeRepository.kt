c00e136394944c28bb1845e504da34cfdfc189a0
oliwier <oliviersakwa@gmail.com>
2021-12-13 22:11:52 +0100

    Home Fragment Repository class

--- app/src/main/java/com/example/bookinghotel/ui/dashboard/home/HomeRepository.kt
+++ app/src/main/java/com/example/bookinghotel/ui/dashboard/home/HomeRepository.kt
@@ -1,18 +1,21 @@
 package com.example.bookinghotel.ui.dashboard.home
 
+import com.example.bookinghotel.domain.models.HotelRoom
+import com.example.bookinghotel.domain.repositories.HotelRoomRepository
 import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.Source
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
 
-class HomeRepository {
-    private val db = Firebase.firestore
-    private val source = Source.CACHE
+class HomeRepository @Inject constructor(
+    private val hotelRoomRepository : HotelRoomRepository
+){
 
-    //Async Function get Hotels from firestore collection named hotel
-    suspend fun getHotels(): QuerySnapshot = db.collection("hotel").get(source).await()
+    suspend fun findAllHotelRoom(): MutableList<HotelRoom> {
+        return hotelRoomRepository.findAllHotelRooms()
+    }
 
-    //Async function get Rooms from firestore collection named room
-    suspend fun getRooms(): QuerySnapshot = db.collection("room").get(source).await()
 }
