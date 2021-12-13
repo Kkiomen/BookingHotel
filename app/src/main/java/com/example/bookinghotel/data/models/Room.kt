afb985692efeb8b63bcd81bf3dbef0244ef4ec6f
oliwier <oliviersakwa@gmail.com>
2021-12-13 22:14:48 +0100

    Firebase Room Model Class

--- app/src/main/java/com/example/bookinghotel/data/models/Room.kt
+++ app/src/main/java/com/example/bookinghotel/data/models/Room.kt
@@ -1,12 +1,9 @@
 package com.example.bookinghotel.data.models
 
 data class Room(
-    val uid: Int? = null,
     val generatedKey : String? = null,
-    val name: String? = null,
-    val persons : String? = null
-    //val availability: Boolean = false,
+    val number: String? = null,
+    val persons : String? = null,
+    val availability: Boolean = false,
     //val description: String? = null,
-    //val countBed: Int? = null,
-    //val idHotel: Int? = null,
 )
