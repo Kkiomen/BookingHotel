e4aa6306343170f52741cf05a2e4cfd5ff0f3584
oliwier <oliviersakwa@gmail.com>
2021-12-13 22:18:03 +0100

    Repository for Room Data Model Class (findAll() etc...)

--- app/src/main/java/com/example/bookinghotel/data/repostiories/RoomRepository.kt
+++ app/src/main/java/com/example/bookinghotel/data/repostiories/RoomRepository.kt
@@ -1,6 +1,7 @@
 package com.example.bookinghotel.data.repostiories
 
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.Source
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -14,8 +15,8 @@ class RoomRepository {
     suspend fun findRoomById(hotelDocumentId : String): DocumentSnapshot? {
         return firestore.collection("room").document(hotelDocumentId).get(source).await()
     }
-    suspend fun findAllRooms(){
-        firestore.collection("room").get(source).await()
+    suspend fun findAllRooms(): QuerySnapshot? {
+        return firestore.collection("room").get(source).await()
     }
 
     fun addRoom(documentId : Int){
