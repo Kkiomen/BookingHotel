634748150422362c409de8d86429ef7b6ebc6f39
oliwier <oliviersakwa@gmail.com>
2021-12-20 23:00:58 +0100

    init commit

--- app/src/main/java/com/example/bookinghotel/ui/dashboard/reservation/ReservationFragment.kt
+++ app/src/main/java/com/example/bookinghotel/ui/dashboard/reservation/ReservationFragment.kt
@@ -1,8 +1,26 @@
 package com.example.bookinghotel.ui.dashboard.reservation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.bookinghotel.databinding.FragmentHomeBinding
+import com.example.bookinghotel.ui.dashboard.home.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ReservationFragment : Fragment() {
+
+    private val viewModel : ReservationViewModel by viewModels()
+
+    private lateinit var binding : FragmentHomeBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
 }
--- app/src/main/java/com/example/bookinghotel/ui/dashboard/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/bookinghotel/ui/dashboard/settings/SettingsFragment.kt
@@ -1,8 +1,26 @@
 package com.example.bookinghotel.ui.dashboard.settings
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.bookinghotel.databinding.FragmentHomeBinding
+import com.example.bookinghotel.ui.dashboard.reservation.ReservationViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SettingsFragment : Fragment() {
+
+    private val viewModel : SettingsViewModel by viewModels()
+
+    private lateinit var binding : FragmentHomeBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
 }
