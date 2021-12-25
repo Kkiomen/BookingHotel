e09d4799eb261903c2179792ac9d0f6b3dd54b33
oliwier <oliviersakwa@gmail.com>
2021-12-25 20:47:49 +0100

    init commit

---
+++ app/src/main/java/com/example/bookinghotel/ui/dashboard/reservation/detailed_reservation_information/DetailedInformationActivity.kt
@@ -0,0 +1,28 @@
+package com.example.bookinghotel.ui.dashboard.reservation.detailed_reservation_information
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bookinghotel.databinding.ActivityDetailedInformationBinding
+import com.example.bookinghotel.databinding.ActivityReservationDetailedInformationBinding
+import com.example.bookinghotel.domain.model.HotelSingleRoom
+import com.example.bookinghotel.domain.model.UserReservedRoom
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailedReservationInformationActivity : AppCompatActivity() {
+    private val viewModel : DetailedReservationInformationViewModel by viewModels()
+    private lateinit var binding : ActivityReservationDetailedInformationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityReservationDetailedInformationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.userReservedRoom = (intent.getSerializableExtra("UserRoom") as UserReservedRoom?)!!
+
+        binding.testTextView.text = viewModel.userReservedRoom.toString()
+    }
+
+}
