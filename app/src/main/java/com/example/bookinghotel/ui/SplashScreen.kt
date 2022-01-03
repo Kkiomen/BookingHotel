a4f79c2e2ea9a9cdf074ec83ffc4e09ce298c56c
Kuba-Brozek <kubabrozek9@gmail.com>
2022-01-03 13:01:40 +0100

    progressBar added to SplashScreen

--- app/src/main/java/com/example/bookinghotel/ui/SplashScreen.kt
+++ app/src/main/java/com/example/bookinghotel/ui/SplashScreen.kt
@@ -1,11 +1,15 @@
 package com.example.bookinghotel.ui
 
+import android.animation.ObjectAnimator
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bookinghotel.R
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 @Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
@@ -13,15 +17,25 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+
+        val circularProgressBar = findViewById<CircularProgressBar>(R.id.circularProgressBar)
+        circularProgressBar.apply {
+
+            setProgressWithAnimation(200f, 3000)
+            progressMax = 200f
+            progressBarColor = Color.WHITE
+            progressBarColorDirection = CircularProgressBar.GradientDirection.TOP_TO_BOTTOM
+
+            backgroundProgressBarColor = Color.GRAY
+            backgroundProgressBarColorDirection = CircularProgressBar.GradientDirection.TOP_TO_BOTTOM
+            progressDirection = CircularProgressBar.ProgressDirection.TO_RIGHT
+        }
 
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000)
+        }, 3000)
     }
 }
+
